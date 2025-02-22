baddf570aa8b66dd5204d492f3e0070fa71a4af0
Daniel <ochigbodaniel240@gmail.com>
2025-02-23 00:18:27 +0100

    test

--- route.js
+++
@@ -1,7 +0,0 @@
-const route = require('express').Router()
-const sendToTelex = require("./controller")
-
-
-route.get('/integration', sendToTelex.telexIntegration)
-route.get('/youtube/content/tick', sendToTelex.sendToTelex)
-module.exports = route
